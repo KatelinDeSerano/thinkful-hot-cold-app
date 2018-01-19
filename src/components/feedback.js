58603a4c05c693f737ab057bdc9699cbd58604f6
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-18 21:50:03 -0600

    created Feedback component

---
+++ src/components/feedback.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './feedback.css';
+
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div className="game-feedback">
+        <h1>This is feedback</h1>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
--- src/components/game.js
+++ src/components/game.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './game.css';
 import Header from './header.js';
+import Feedback from './feedback.js';
 
 class Game extends Component {
   render() {
     return (
       <div className="game">
         <Header /> 
+        <h1>HOT or COLD</h1>
+        <Feedback />
+
       </div>
     );
   }
