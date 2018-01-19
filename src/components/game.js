5a245de182bba8807c8e6112af85fd0935bd0b41
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-18 21:53:35 -0600

    created Guess component

--- src/components/game.js
+++ src/components/game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './game.css';
 import Header from './header.js';
 import Feedback from './feedback.js';
+import Guess from './guess.js';
 
 class Game extends Component {
   render() {
@@ -10,6 +11,7 @@ class Game extends Component {
         <Header /> 
         <h1>HOT or COLD</h1>
         <Feedback />
+        <Guess />
 
       </div>
     );
---
+++ src/components/guess.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './guess.css';
+
+
+class Guess extends Component {
+  render() {
+    return (
+      <div className="game-guess">
+        <h1>This is the Guess Form</h1>
+      </div>
+    );
+  }
+}
+
+export default Guess;
